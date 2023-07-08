4fb29f96b54e30536e850b3ebd03da2381fdb0ed
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2023-07-08 22:17:22 +0300

    security config

---
+++ src/main/java/org/obukhova/ssr/configs/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package org.obukhova.ssr.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
