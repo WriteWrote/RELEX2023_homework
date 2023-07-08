a939bfc4fd4e1d25c0d9ca7e7d9084397aede633
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2023-07-08 23:28:55 +0300

    RoleRepository and UserRepository

--- src/main/java/org/obukhova/ssr/repository/RoleRepository.java
+++ src/main/java/org/obukhova/ssr/repository/RoleRepository.java
@@ -1,11 +1,12 @@
 package org.obukhova.ssr.repository;
 
-import org.obukhova.ssr.model.entity.MessageEntity;
+import org.obukhova.ssr.model.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MessageRepository extends JpaRepository<MessageEntity, Integer> {
+public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
+    Optional<RoleEntity> findByName(String name);
 }
---
+++ src/main/java/org/obukhova/ssr/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package org.obukhova.ssr.repository;
+
+import org.obukhova.ssr.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+}
